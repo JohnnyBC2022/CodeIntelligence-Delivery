256fe1d0167ace2c4f307a1c7b0cb9910a9f7f6c
Johnny <jonathanbc2012@gmail.com>
2024-10-21 20:17:09 +0200

    feat: TruckDriverController updated. Added try-catch to allmethods

--- src/main/java/com/codeintelligence/delivery/controller/TruckDriverController.java
+++ src/main/java/com/codeintelligence/delivery/controller/TruckDriverController.java
@@ -13,83 +13,95 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
- * REST Controller for managing truck drivers.
+ * Controller for managing truck driver operations.
  */
 @RestController
 @RequestMapping("/api/truck-drivers")
 public class TruckDriverController {
+
     private final TruckDriverService truckDriverService;
 
-    /**
-     * Constructs a TruckDriverController with the specified TruckDriverService.
-     *
-     * @param truckDriverService the service used to perform CRUD operations on truck drivers     *
-     */
     @Autowired
-    public TruckDriverController(TruckDriverService truckDriverService){
+    public TruckDriverController(TruckDriverService truckDriverService) {
         this.truckDriverService = truckDriverService;
     }
 
     /**
-     * Handles the creation of a new truck driver.
+     * Creates a new truck driver.
      *
-     * @param truckDriverDTO the DTO containing the information of the truck driver to be created
-     * @return a ResponseEntity containing the created TruckDriverDTO and the HTTP status code
+     * @param truckDriverDTO the DTO of the truck driver to create
+     * @return ResponseEntity with the created truck driver's DTO and HTTP status 201 (CREATED)
      */
-    @PostMapping(value = "save")
+    @PostMapping(value = "/save")
     public ResponseEntity<TruckDriverDTO> createTruckDriver(@RequestBody TruckDriverDTO truckDriverDTO) {
-        TruckDriverEntity truckDriver = convertToEntity(truckDriverDTO);
-        TruckDriverEntity createdTruckDriver = truckDriverService.saveTruckDriver(truckDriver);
-        return new ResponseEntity<>(convertToDTO(createdTruckDriver), HttpStatus.CREATED);
+        try {
+            TruckDriverEntity truckDriver = convertToEntity(truckDriverDTO);
+            TruckDriverEntity createdTruckDriver = truckDriverService.saveTruckDriver(truckDriver);
+            return new ResponseEntity<>(convertToDTO(createdTruckDriver), HttpStatus.CREATED);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     /**
-     * Retrieves a list of all truck drivers.
+     * Retrieves all truck drivers.
      *
-     * @return a ResponseEntity containing a list of TruckDriverDTOs and the HTTP status code
+     * @return ResponseEntity with the list of truck driver DTOs and HTTP status 200 (OK)
      */
     @GetMapping
     public ResponseEntity<List<TruckDriverDTO>> getAllTruckDrivers() {
-        List<TruckDriverEntity> truckDrivers = truckDriverService.findAllTruckDrivers();
-        List<TruckDriverDTO> truckDriverDTOs = truckDrivers.stream()
-                .map(this::convertToDTO)
-                .collect(Collectors.toList());
-        return new ResponseEntity<>(truckDriverDTOs, HttpStatus.OK);
+        try {
+            List<TruckDriverEntity> truckDrivers = truckDriverService.findAllTruckDrivers();
+            List<TruckDriverDTO> truckDriverDTOs = truckDrivers.stream()
+                    .map(this::convertToDTO)
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(truckDriverDTOs, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
-     * Finds a truck driver by their ID.
+     * Retrieves a truck driver by their ID.
      *
-     * @param id The ID of the truck driver to find.
-     * @return The response with the found truck driver.
+     * @param id the ID of the truck driver
+     * @return ResponseEntity with the found truck driver's DTO or HTTP status 404 (NOT FOUND)
      */
     @GetMapping("/get/{id}")
-    public ResponseEntity<TruckDriverDTO> getTruckDriverById(@PathVariable Long id) { // Cambiado de dni a id
-        Optional<TruckDriverEntity> truckDriver = truckDriverService.findTruckDriverById(id);
-        return truckDriver.map(driver -> new ResponseEntity<>(convertToDTO(driver), HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public ResponseEntity<TruckDriverDTO> getTruckDriverById(@PathVariable Long id) {
+        try {
+            Optional<TruckDriverEntity> truckDriver = truckDriverService.findTruckDriverById(id);
+            return truckDriver.map(driver -> new ResponseEntity<>(convertToDTO(driver), HttpStatus.OK))
+                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
      * Updates a truck driver by their ID.
      *
-     * @param id The ID of the truck driver to update.
-     * @param truckDriverDTO The DTO object of the truck driver with new data.
-     * @return The response with the updated truck driver.
+     * @param id             the ID of the truck driver to update
+     * @param truckDriverDTO the updated truck driver's DTO
+     * @return ResponseEntity with the updated truck driver's DTO or HTTP status 404 (NOT FOUND)
      */
     @PutMapping("/update/{id}")
     public ResponseEntity<TruckDriverDTO> updateTruckDriverById(@PathVariable Long id, @RequestBody TruckDriverDTO truckDriverDTO) {
-        TruckDriverEntity truckDriver = convertToEntity(truckDriverDTO);
-        Optional<TruckDriverEntity> updatedTruckDriver = truckDriverService.updateTruckDriverById(id, truckDriver);
-        return updatedTruckDriver.map(driver -> new ResponseEntity<>(convertToDTO(driver), HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        try {
+            TruckDriverEntity truckDriver = convertToEntity(truckDriverDTO);
+            Optional<TruckDriverEntity> updatedTruckDriver = truckDriverService.updateTruckDriverById(id, truckDriver);
+            return updatedTruckDriver.map(driver -> new ResponseEntity<>(convertToDTO(driver), HttpStatus.OK))
+                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
      * Deletes a truck driver by their ID.
      *
-     * @param id The ID of the truck driver to delete.
-     * @return The response for the deletion.
+     * @param id the ID of the truck driver to delete
+     * @return ResponseEntity with HTTP status 204 (NO CONTENT) if deletion was successful
      */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteTruckDriverById(@PathVariable Long id) {
@@ -101,13 +113,13 @@ public class TruckDriverController {
         }
     }
 
-    // Métodos de conversión de entidades
+    // Conversion methods between entities
 
     /**
-     * Converts a TruckDriverEntity to a TruckDriverDTO.
+     * Converts a truck driver entity to its corresponding DTO.
      *
-     * @param truckDriver the TruckDriverEntity object to convert
-     * @return the corresponding TruckDriverDTO object
+     * @param truckDriver the truck driver entity
+     * @return the truck driver DTO
      */
     private TruckDriverDTO convertToDTO(TruckDriverEntity truckDriver) {
         TruckDriverDTO dto = new TruckDriverDTO();
@@ -121,10 +133,10 @@ public class TruckDriverController {
     }
 
     /**
-     * Converts a TruckDriverDTO to a TruckDriverEntity.
+     * Converts a truck driver DTO to its corresponding entity.
      *
-     * @param dto the TruckDriverDTO object to convert
-     * @return the corresponding TruckDriverEntity object
+     * @param dto the truck driver DTO
+     * @return the truck driver entity
      */
     private TruckDriverEntity convertToEntity(TruckDriverDTO dto) {
         TruckDriverEntity truckDriver = new TruckDriverEntity();
