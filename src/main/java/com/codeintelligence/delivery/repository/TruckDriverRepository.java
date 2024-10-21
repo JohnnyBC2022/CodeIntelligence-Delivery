7e0bb8c408d16ac57ed0afa417682dbb8cfa1721
Johnny <jonathanbc2012@gmail.com>
2024-10-21 11:03:46 +0200

    feat: TruckDriverRepository created

---
+++ src/main/java/com/codeintelligence/delivery/repository/TruckDriverRepository.java
@@ -0,0 +1,13 @@
+package com.codeintelligence.delivery.repository;
+
+import com.codeintelligence.delivery.model.truckdriver.TruckDriverEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository interface for the TruckDriver entity.
+ * Provides methods to perform CRUD operations on truck drivers.
+ */
+@Repository
+public interface TruckDriverRepository extends JpaRepository<TruckDriverEntity,Long> {
+}
