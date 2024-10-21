a3e7fb8987b2963c1643c779cba2149e8354bd0f
Johnny <jonathanbc2012@gmail.com>
2024-10-21 11:54:39 +0200

    feat: TruckDriverService created

--- src/main/java/com/codeintelligence/delivery/service/TruckDriverService.java
+++ src/main/java/com/codeintelligence/delivery/service/TruckDriverService.java
@@ -1,4 +1,24 @@
 package com.codeintelligence.delivery.service;
 
+import com.codeintelligence.delivery.model.truckdriver.TruckDriverEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service interface for managing truck drivers
+ */
+
 public interface TruckDriverService {
+
+    TruckDriverEntity saveTruckDriver(TruckDriverEntity truckDriver);
+
+    List<TruckDriverEntity> findAllTruckDrivers();
+
+    Optional<TruckDriverEntity> findByDni(String dni);
+
+    Optional<TruckDriverEntity> updateTruckDriverByDni(String dni, TruckDriverEntity truckDriver);
+
+    void deleteByDni(String dni);
+
 }
